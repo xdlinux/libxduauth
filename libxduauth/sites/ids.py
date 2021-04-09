1323beeb1d66420d610e53423212b01e209dd269
Frank <frankli0324@hotmail.com>
2021-04-09 11:34:52 +0800

    [ids] in case captcha is needed

--- libxduauth/sites/ids.py
+++ libxduauth/sites/ids.py
@@ -1,7 +1,12 @@
+import time
+from io import BytesIO
+
+from PIL import Image
 from bs4 import BeautifulSoup
 
 from ..AuthSession import AuthSession
 from ..utils.page import parse_form_hidden_inputs
+from ..utils.vcode import _process_vcode
 from ..utils.aes import encrypt
 
 
@@ -21,6 +26,17 @@ class IDSSession(AuthSession):
             'http://ids.xidian.edu.cn/authserver/login',
             params={'service': target, 'type': 'userNameLogin'}
         ).text
+        is_need_captcha, vcode = self.get(
+            'https://ids.xidian.edu.cn/authserver/checkNeedCaptcha.htl',
+            params={'username': username, '_': int(time.time() * 1000)}
+        ).json()['isNeed'], None
+        if is_need_captcha:
+            captcha = self.get(
+                'http://ids.xidian.edu.cn/authserver/getCaptcha.htl',
+                params={str(int(time.time() * 1000)): ''}
+            )
+            _process_vcode(Image.open(BytesIO(captcha.content))).show()
+            vcode = input('验证码：')
         page = BeautifulSoup(page, "lxml")
         form = page.findChild(attrs={'id': 'pwdFromId'})
         params = parse_form_hidden_inputs(form)
@@ -31,7 +47,8 @@ class IDSSession(AuthSession):
             data=dict(params, **{
                 'username': username,
                 'password': encrypt(password.encode(), enc.encode()),
-                'rememberMe': 'on'
+                'captcha': vcode,
+                'rememberMe': 'true'
             })
         )
 
