5217ab686800e6bc01a80e48f3fb3f5f3fe5fe94
Frank <frankli0324@hotmail.com>
2023-09-04 19:49:55 +0800

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="libxduauth",
-    version="1.7.4",
+    version="1.7.5",
     author="Frank",
     author_email="frankli0324@hotmail.com",
     description="login utilities for XDU",
