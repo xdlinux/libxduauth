5be1d698bacf963bd225258db9f597a6f6bf6f61
Null <pairmanxlr@gmail.com>
2024-03-06 09:19:20 +0800

    reintroduce captcha detection

--- libxduauth/sites/ids.py
+++ libxduauth/sites/ids.py
@@ -25,32 +25,38 @@ class IDSSession(AuthSession):
             'http://ids.xidian.edu.cn/authserver/login',
             params={'service': target}
         ).text
-        captcha = self.get(
-            'https://ids.xidian.edu.cn/authserver/common/openSliderCaptcha.htl',
-            params={'_': int(time.time() * 1000)}
-        )
-        # 返回: {
-        #     'bigImage': ..., # 背景图(base64)
-        #     'smallImage': ..., # 滑块图(base64)
-        #     'tagWidth": 93, # 无用, 恒93
-        #     'yHeight': 0 # 无用, 恒0
-        # }
-        img = Image.open(BytesIO(captcha.json()['bigImage']))
-        img.show()
-        # move_len: 背景图左侧到滑块目标位置左侧的宽度
-        move_len = input('滑块位移:')
-        # canvasLength: canvas宽度, 硬编码280
-        # moveLength: 按比例缩放后的滑块位移, 有容错
-        verify = self.post(
-            'https://ids.xidian.edu.cn/authserver/common/verifySliderCaptcha.htl',
-            data={'canvasLength': 280,
-                  'moveLength': move_len * 280 // img.width
-            }
-        )
-        # 返回: {
-        #     'errorCode': ..., # 验证通过时为1
-        #     'errorMsg': ... # 验证通过时为'success'
-	# }
+	is_need_captcha = self.get(
+            'https://ids.xidian.edu.cn/authserver/checkNeedCaptcha.htl',
+            params={'username': username, '_': int(time.time() * 1000)}
+        ).json()['isNeed']
+	if is_need_captcha:
+            captcha = self.get(
+                'https://ids.xidian.edu.cn/authserver/common/openSliderCaptcha.htl',
+                params={'_': str(int(time.time() * 1000))}
+            )
+            # 返回: {
+            #     'bigImage': ..., # 背景图(base64)
+            #     'smallImage': ..., # 滑块图(base64)
+            #     'tagWidth": 93, # 无用, 恒93
+            #     'yHeight': 0 # 无用, 恒0
+            # }
+            img = Image.open(BytesIO(captcha.json()['bigImage']))
+            img.show()
+            # move_len: 背景图左侧到滑块目标位置左侧的宽度
+            move_len = input('滑块位移:')
+            # canvasLength: canvas宽度, 硬编码280
+            # moveLength: 按比例缩放后的滑块位移, 有容错
+            verify = self.post(
+                'https://ids.xidian.edu.cn/authserver/common/verifySliderCaptcha.htl',
+                data={
+                      'canvasLength': '280',
+                      'moveLength': str(move_len * 280 // img.width)
+                }
+            )
+            # 返回: {
+            #     'errorCode': ..., # 验证通过时为1
+            #     'errorMsg': ... # 验证通过时为'success'
+	    # }
         page = BeautifulSoup(page, "lxml")
         form = page.findChild(attrs={'id': 'pwdFromId'})
         params = parse_form_hidden_inputs(form)
@@ -62,7 +68,7 @@ class IDSSession(AuthSession):
                 'username': username,
                 'password': encrypt(password.encode(), enc.encode()),
                 'captcha': '',
-                'rememberMe': True
+                'rememberMe': 'true'
             })
         )
 
