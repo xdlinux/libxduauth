3bcf8d8426e6e3b0ee9d0f8cd5da01a49e7605f9
Null <pairmanxlr@gmail.com>
2024-03-09 17:22:49 +0800

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="libxduauth",
-    version="1.8.0",
+    version="1.8.1",
     author="Frank",
     author_email="frankli0324@hotmail.com",
     description="login utilities for XDU",
