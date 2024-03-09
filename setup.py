a3f1d3f0d004c040474fdc8ca636ca2aa89afe60
Null <pairmanxlr@gmail.com>
2024-03-09 17:21:33 +0800

    fix project homepage url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="login utilities for XDU",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/xdlinux/libxdauth",
+    url="https://github.com/xdlinux/libxduauth",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
